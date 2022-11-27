6f90be530ef02c1e1aba7f1a785f7adb70db27ae
unknown <girgink@gmail.com>
2022-11-27 13:35:04 +0100

    Added Diff class documentation

--- src/fairly/diff.py
+++ src/fairly/diff.py
@@ -1,3 +1,14 @@
+"""Diff class module.
+
+Diff class is used to keep track of dataset modifications.
+
+Usage example:
+
+    >>> diff = Diff()
+    >>> diff.modify("name", "Johnny", "John")
+    >>> diff.modified
+        {"name": ("Johnny", "John")}
+"""
 from typing import Dict
 
 class Diff:
@@ -24,26 +35,48 @@ class Diff:
 
     @property
     def added(self) -> Dict:
+        """Returns a dictionary of added items."""
         return self._added
 
 
     @property
     def modified(self) -> Dict:
+        """Returns a dictionary of modified items."""
         return self._modified
 
 
     @property
     def removed(self) -> Dict:
+        """Returns a dictionary of removed items."""
         return self._removed
 
 
     def add(self, key, val) -> None:
+        """Appends an item to the diff set as added.
+
+        Args:
+            key: Item key
+            val: Item value
+        """
         self._added[key] = val
 
 
     def modify(self, key, val, oldval) -> None:
+        """Appends an item to the diff set as modified.
+
+        Args:
+            key: Item key
+            val: Item value
+            oldVal: Old value of the item
+        """
         self._modified[key] = (val, oldval)
 
 
     def remove(self, key, val) -> None:
+        """Appends an item to the diff set as removed.
+
+        Args:
+            key: Item key
+            val: Item value
+        """
         self._removed[key] = val
