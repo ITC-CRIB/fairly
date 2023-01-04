a86c5396ea550d34761ff32e544c8065952c2497
jurra <jose.urra86@gmail.com>
2023-01-04 10:51:55 +0100

    Status of issues and Todos

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -7,7 +7,7 @@ import cli.repository
 
 app = typer.Typer()
 app.add_typer(cli.dataset.app, name="dataset")
-app.add_typer(cli.repository.app, name="repo")
+app.add_typer(cli.repository.app, name="config")
 
 if __name__ == "__main__":
     app()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ from typer.testing import CliRunner
 import fairly
 from fairly import LocalDataset
 
-from cli.main import app
+from cli import app
 from cli.repository import app as repo_app
 from cli.dataset import app as dataset_app
 
@@ -23,10 +23,11 @@ def test_dataset_create(cli_testing_tempdir):
     '''The dataset creation with the cli simply means creating a manifest file
     in the directory that wants to be turned into a dataset'''
     # Create a manifest file from the template in the current working directory
-    runner.invoke(dataset_app, ["create", "zenodo"])
+    runner.invoke(app, ["dataset", "create", "zenodo"])
 
+    print(f"Working directory: {os.getcwd()}")
     # Create dummmy manifest file
-    create_manifest_from_template("zenodo.yaml", cli_testing_tempdir)
+    create_manifest_from_template("../src/fairly/data/templates/","zenodo.yaml", cli_testing_tempdir)
 
     # Reads the manifest file that makes the directory a dataset'
     dataset = fairly.dataset(cli_testing_tempdir.strpath)
@@ -49,42 +50,42 @@ def test_dataset_upload():
     # stire the dataset metadata in the manifest
     pass
 
-# vcr decorator
+def gen_dataset_path(dataset):
+    '''Generates a path name for a dataset directory in the current working directory
+    based on the title of the dataset
+    '''
+    return dataset.metadata['title'].replace(' ', '_')
+
+# Here we define a fixed dataset that will be used by the tests bellow
+CLIENT = fairly.client("zenodo")
+DATASET = fairly.dataset('./tests/fixtures/dummy_dataset')
+REMOTE_DATASET = DATASET.upload(CLIENT.client_id, notify=fairly.notify)
+
+DATASET_COMMANDS = [
+    # fairly dataset clone --url <url>
+    ['dataset', 'clone', '--url', 'https://zenodo.org/record/1234567', 'tmp_1'],
+
+    # fairly dataset clone --doi <doi>
+    # TODO: method not implemented yet in fairly package
+    # ['dataset', 'clone', '--doi', '10.5281/zenodo.1234567'],
+
+    # fairly dataset clone --repo <repo> --id <id>
+    ['dataset', 'clone', '--repo', 'zenodo', '--id', REMOTE_DATASET.id['id'], 'tmp_2'], 
+]
+
 @pytest.mark.vcr(cassette_library_dir="tests/fixtures/vcr_cassettes_cli",
-                filter_headers=["authorization"])
-def test_dataset_download():
+                filter_headers=["authorization"], allow_playback_repeats=True)
+@pytest.mark.parametrize('command', DATASET_COMMANDS)
+def test_cli_dataset_clone(command: list, cli_testing_tempdir):
     '''Test the download of a dataset by using its URL address, DOI or ID'''
-    # A private function to test that all commands are working
-    def _check_download(dataset_path, remote):
-        assert os.path.exists(dataset_path) == True
-        assert os.path.exists(f'{dataset_path}/manifest.yaml') == True
-        dataset = fairly.dataset(dataset_path)
-        assert dataset is not None
-        assert isinstance(dataset, LocalDataset)
-        if remote: assert len(remote.files) == len(dataset.files)
-
-        # remove files in the directory
-        for file in os.listdir(dataset_path):
-            os.remove(f'{dataset_path}/{file}')
-        os.rmdir(dataset_path)
-
-    client = fairly.client("zenodo")
+    working_dir = os.getcwd()
+    print(command)
+    # fairly dataset clone --url <url>
+    os.chdir(cli_testing_tempdir.strpath)
+    result = runner.invoke(app, command)
+    # for positive download operations is good enough to check that the command
+    # was executed without errors
+    assert result.exit_code == 0
     
-    # Create a dummy dataset
-    dataset = fairly.dataset('./tests/fixtures/dummy_dataset')
-    
-    # Upload the dataset
-    remote_dataset = dataset.upload(client.client_id, notify=fairly.notify)
-    assert remote_dataset is not None
-    assert len(remote_dataset.files) == len(dataset.files)
-
-    # fairly dataset clone <url|doi>
-    runner.invoke(app, ["dataset", "clone", "--doi", "10.5281/zenodo.3929547"])
-    _check_download()
-
-    # runner.invoke(app, ["dataset", "clone", "--url", remote_dataset.url])
-    # _check_download()
-
-    # fairly dataset clone --repo zenodo --id 6026285
-    runner.invoke(app, ["dataset", "clone", "--repo", "zenodo" , "--id", remote_dataset.id['id']]) 
-    _check_download()
+    # go back to the original working directory
+    os.chdir(working_dir)
