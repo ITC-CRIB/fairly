d9e330a871c8ef01966ba1c12ddda59deafdf1ad
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-03-17 10:47:50 +0100

    update test files with verision on cli branch

---
+++ tests/__init__.py
@@ -0,0 +1,76 @@
+import os
+import yaml
+import json
+import uuid
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import vcr
+
+import pytest
+import fairly
+
+# Requires develop to have .env file with FAIRLY_FIGSHARE_TOKEN
+FIGSHARE_TOKEN = os.environ.get("FAIRLY_FIGSHARE_TOKEN")
+ZENODO_TOKEN = os.environ.get("FAIRLY_ZENODO_TOKEN")
+
+# load clients from supported clients
+TEMPLATES = os.listdir("./src/fairly/data/templates")
+TEMPLATES_DIR = "./src/fairly/data/templates/"
+ROOT_DIR = os.getcwd()
+
+# We generate a unique string that we can use to populate metadata for testing
+ustring = str(uuid.uuid4())
+
+# Create a file in ~./fairly/config.json
+with open(os.path.expanduser("~/.fairly/config.json"), "w") as f:
+    # Create dict with config
+    config = { "4tu": { "token": FIGSHARE_TOKEN }, 
+               "zenodo": { "token": ZENODO_TOKEN } 
+             }
+    f.write(json.dumps(config))
+
+def create_manifest_from_template(template_path: str, template_file: str,  target_path) -> None:
+    """Create a manifest file from a template file
+    Parameters
+    ----------
+    template_path : str
+        Path
+
+    template_file : str
+        Name of the template file in yaml format e.g. figshare.yaml
+        the file is extracted from the templates folder
+
+    target_path : str
+        Path to the target folder where the manifest file will be created
+
+    Example
+    -------
+    >>> create_manifest_from_template("./src/fairly/data/templates/", "figshare.yaml", "./tests/data/")
+    """
+    with open(f"{template_path}/{template_file}", "r") as f:
+        template = f.read()
+        template = yaml.safe_load(template)
+        template['metadata']['title'] = "My fairly test"
+        template['metadata']['description'] = ustring
+        # Add files key to the manifest so that files are added to the dataset object
+        template['files'] = { 'excludes': [], 'includes': ["*.txt"] }
+        if template_file == "figshare.yaml":
+            template['metadata']['authors'] = [ ustring ]
+        if template_file == "zenodo.yaml":
+            template['metadata']['creators'] = [ { "name": ustring } ]
+            template['metadata']['authors'] = [ {"name" : ustring } ]
+            template['metadata']['description'] = ustring
+            template['metadata']['license'] = 'cc-by-nc-4.0'
+            template['metadata']['type'] = 'dataset'
+            # template dates
+            template['metadata']['publication_date'] = '2020-01-01'
+
+    with open(f"{target_path}/manifest.yaml", "w") as f:
+        f.write(yaml.dump(template))
+
+# Set testing flag
+fairly.TESTING = True
+
+
--- tests/test_fairly.py
+++ tests/test_fairly.py
@@ -1,4 +1,5 @@
 import os
+import json
 from ruamel.yaml import YAML
 import shutil
 from functools import lru_cache
@@ -10,6 +11,8 @@ import fairly
 from fairly.dataset import Dataset
 from fairly.client.figshare import FigshareClient
 from fairly.client.zenodo import ZenodoClient
+
+from tests import create_manifest_from_template
 
 # Set testing flag
 fairly.TESTING = True
@@ -29,44 +32,11 @@ def params_create_client():
         ("figshare", FigshareClient),
         ("zenodo", ZenodoClient)
     ]
-
-
-def create_manifest_from_template(template_file: str, path) -> None:
-    """Create a manifest file from a template file
-    This procedure fills the manifest with the minimum required metadata to create a remote dataset
-
-    Parameters
-    ----------
-    template_file : str
-        Name of the template file in yaml format e.g. figshare.yaml
-        the file is extracted from the templates folder
-    """
-    with open(f"./src/fairly/data/templates/{template_file}", "r") as file:
-        yaml = YAML()
-        template = yaml.load(file)
-        template['metadata']['title'] = "My fairly test"
-        template['metadata']['description'] = "My test description"
-        # Add files key to the manifest so that files are added to the dataset object
-        template['files'] = { 'excludes': [], 'includes': ["*.txt"] }
-        if template_file == "figshare.yaml":
-            template['metadata']['authors'] = [ "John Doe" ]
-        if template_file == "zenodo.yaml":
-            template['metadata']['creators'] = [ { "name": "John Doe" } ]
-            template['metadata']['authors'] = [ {"name" : "John Doe" } ]
-            template['metadata']['description'] = "My test description"
-            template['metadata']['license'] = 'cc-by-nc-4.0'
-            template['metadata']['type'] = 'dataset'
-            # template dates
-            template['metadata']['publication_date'] = '2020-01-01'
-
-    with open(f"{path}/manifest.yaml", "w") as file:
-        yaml.dump(template, file)
  
-
 def test_load_config():
     config = fairly.get_config("zenodo")
     assert config is not None
-    assert config["url"] == "https://zenodo.org/"
+    assert config["token"] == os.environ.get("FAIRLY_ZENODO_TOKEN")
 
 
 def test_get_clients():
@@ -88,7 +58,7 @@ def test_create_client(client_id, client_class):
 
 @pytest.mark.vcr(cassette_library_dir='tests/fixtures/vcr_cassettes', filter_headers=['authorization'])
 @pytest.mark.parametrize("client", params_clients())
-def test_create_and_upload_dataset(client: fairly.Client, dummy_dataset):
+def test_create_and_upload_dataset(client: fairly.Client, templates, dummy_dataset):
     """
     Test the procedure of creating a local dataset and uploading it to various
     remote repositories.
@@ -99,7 +69,7 @@ def test_create_and_upload_dataset(client: fairly.Client, dummy_dataset):
 
     # This copies the template for the specific client 
     # and writes it to the dummy dataset directory
-    create_manifest_from_template(f"{client.client_id}.yaml", dummy_dataset)
+    create_manifest_from_template(templates, f"{client.client_id}.yaml", dummy_dataset)
 
     local_dataset = fairly.dataset(dummy_dataset)
     assert local_dataset is not None
@@ -108,7 +78,7 @@ def test_create_and_upload_dataset(client: fairly.Client, dummy_dataset):
 
     # Notify user that token is not set
     with pytest.raises(ValueError):
-        tokenless_client = fairly.client(client.client_id)
+        tokenless_client = fairly.client(id='zenodo', token=None)
         local_dataset.upload(tokenless_client)
 
     remote_dataset = local_dataset.upload(client, notify=fairly.notify)
@@ -121,11 +91,11 @@ def test_create_and_upload_dataset(client: fairly.Client, dummy_dataset):
 
 @pytest.mark.vcr(cassette_library_dir='tests/fixtures/vcr_cassettes', filter_headers=['authorization'])
 @pytest.mark.parametrize("client", params_clients())
-def test_download_dataset(client: fairly.Client, dummy_dataset):
+def test_download_dataset(client: fairly.Client, templates, dummy_dataset):
     """Test the download of the different datasets created."""
     # Local dataset is created in the tests folder
     # and then deleted after the test is done
-    create_manifest_from_template(f"{client.client_id}.yaml", dummy_dataset)
+    create_manifest_from_template(templates, f"{client.client_id}.yaml", dummy_dataset)
 
     local_dataset = fairly.dataset(dummy_dataset)
 
